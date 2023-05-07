20519439f0d88a7d858267edf1b70def242fe578
Evgeniy Kraynik <leniv4ik@gmail.com>
2023-05-07 17:32:29 +0300

    form:radiobuttons

--- src/main/java/com/kraynik/spring/mvc/Employee.java
+++ src/main/java/com/kraynik/spring/mvc/Employee.java
@@ -12,6 +12,7 @@ public class Employee {
     private String carBrand;
     private Map<String, String> carBrands;
     private String[] languages;
+    private Map<String, String> languageList;
 
     public Employee() {
         departments = new HashMap<>();
@@ -23,6 +24,12 @@ public class Employee {
         carBrands.put("BMW", "BMW");
         carBrands.put("Audi", "Audi");
         carBrands.put("MB", "MB");
+
+        languageList = new HashMap<>();
+        languageList.put("Engish", "EN");
+        languageList.put("Deutch", "DE");
+        languageList.put("French", "FR");
+
     }
 
     public String getName() {
@@ -87,6 +94,14 @@ public class Employee {
 
     public void setLanguages(String[] languages) {
         this.languages = languages;
+    }
+
+    public Map<String, String> getLanguageList() {
+        return languageList;
+    }
+
+    public void setLanguageList(Map<String, String> languageList) {
+        this.languageList = languageList;
     }
 
     @Override
--- src/main/java/com/kraynik/spring/mvc/MyController.java
+++ src/main/java/com/kraynik/spring/mvc/MyController.java
@@ -26,13 +26,6 @@ public class MyController {
     @RequestMapping("/showDetails")
     public String showEmpDetails(@ModelAttribute("employee") Employee emp) {
 
-        String name = emp.getName();
-        emp.setName("Mr. " + name);
-        String surname = emp.getSurname();
-        emp.setSurname(surname + "!");
-        int salary = emp.getSalary();
-        emp.setSalary(salary * 10);
-
         return "show-emp-detail-view";
     }
 }
